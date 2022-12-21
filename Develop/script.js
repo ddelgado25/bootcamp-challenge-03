0074d84b99931e1cb706df4f9e133ad1c4af65e6
ddelgado25 <118149533+ddelgado25@users.noreply.github.com>
2022-12-21 02:50:41 -0500

    added variables, created functions and if statements for each password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,12 @@
+// Variables
+var passwordInput =[];
+var passwordLength = 12;
+var numbers = ["1","2","3","4","5","6","7","8","9"];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var specialCharacters = ["!", "\\", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+var passwordDone = [];
+   
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -10,5 +19,38 @@ function writePassword() {
 
 }
 
+function generatePassword(){
+  userPrompts();
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var userInfo = userPrompts();
+
+function userPrompts(){
+  var passwordLength = parseInt(prompt("How long do you want your password to be?"))
+  
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Your password must be between 8 - 128 characters long.")
+    return false
+  }
+
+  if (confirm("Would you like to include lower case letters in your password?")) {
+    passwordInput = passwordInput.concat(lowerCase)
+  }
+
+  if (confirm("Would you like to include upper case letters in your password?")) {
+    passwordInput = passwordInput.concat(upperCase)
+  }
+
+  if (confirm("Would you like to include numbers in your password?")) {
+    passwordInput = passwordInput.concat(numbers)
+  }
+
+  if (confirm("Would you like to include special characters in your password?")) {
+    passwordInput = passwordInput.concat(specialCharacters)
+  }
+  console.log(passwordInput);
+
+}
